a31d9b83615b0b81f0a0188fc589fe0f51561a5f
zhaojunjie <520095417@qq.com>
2014-05-15 17:56:52 +0800

    bilib web ref

---
+++ codeforfun/python/bilibili.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+#ecoding=utf-8
+import requests
+
+def login (user,passwd):
+    print user+passwd
+
+
+def main ():
+    user='520095417@qq.com'
+    passwd='z4326959'
+    login(user,passwd)
+
+if __name__=='__main__':
+    main()
