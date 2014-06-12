f1fe8c27204d089d6dc5d0777b5a17e200497416
zhaojunjie <520095417@qq.com>
2014-06-12 12:15:10 +0800

    add apkDecode shell script

---
+++ mytools/myshell/apkDecode.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#edit by Zjj
+apk=$1
+file=$2
+
+apktool(){
+    echo
+    echo "doing apktool...."
+    echo
+    ~/Documents/mytools/apktool/apktool d $apk $file
+}
+
+class2jar(){
+    echo
+    echo "unzip classes.dex ..."
+    echo
+    unzip $apk classes.dex
+    echo
+    echo "unzip dex2jar classes.dex ..."
+    echo
+    ~/Documents/mytools/apktool/dex2jar/dex2jar.sh classes.dex
+    rm classes.dex
+    mv classes_dex2jar.jar $file/$apk.jar
+}
+
+apktool 
+class2jar
