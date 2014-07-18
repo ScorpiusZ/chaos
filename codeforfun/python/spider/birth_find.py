bb811ff69646253bccc36e9d60df0b2cda734af6
ScorpiusZjj <520095417@qq.com>
2014-07-18 20:53:13 +0800

    add filter file and filter mode

--- codeforfun/python/spider/birth_find.py
+++ codeforfun/python/spider/birth_find.py
@@ -7,9 +7,9 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def get(count):
-    url='http://www.douban.com/group/topic/30388571/?start='
-    url+=str(count*100)
+mode=True
+def get(count,url):
+    url+='?start='+str(count*100)
     try:
         response=requests.get(url,timeout=20)
     except:
@@ -18,32 +18,53 @@ def get(count):
     html=response.text
     soup=BeautifulSoup(html)
     lis=soup.findAll('li',class_='clearfix comment-item')
+    if not lis or not len(lis):
+        return False
     i=0
     for item in lis:
         handle(item,count,i)
         i+=1
     print '%s finish'%(str(count))
+    return True
 
 
 def handle(Item,page,num):
     content=Item.p.string
     content=str(content)
     content=content.strip()
-    if '1105' in content:
-        print 'content %s page = %s num = %s'%(content,page,num)
-    elif '11月5' in content:
-        print 'content %s page = %s num = %s'%(content,page,num)
-    elif '11月05' in content:
-        print 'content %s page = %s num = %s'%(content,page,num)
-    elif '11.5' in content:
-        print 'content %s page = %s num = %s'%(content,page,num)
-    elif '11.05' in content:
-        print 'content %s page = %s num = %s'%(content,page,num)
+    filter_content(content,page,num)
 
+def filter_content(content,page,num):
+    filters=getfilter()
+    isPass=False
+    for filter_item in filters:
+        if not filter_item:
+            continue
+        if filter_item in content:
+            isPass=True
+            if not mode:
+                break
+        else:
+            if mode:
+                isPass=False
+                break;
+    if isPass:
+            print 'content %s page = %s num = %s'%(content,page,num)
+
+
+def getfilter():
+    filters=[]
+    with open('filter','r') as filter_file:
+        contents=filter_file.read().split('\n')
+        for line in contents:
+            filters.append(line)
+    return filters
 
 def main():
-    for i in xrange(178):
-        get(i)
+    url=sys.argv[1]
+    i=0
+    while get(i,url):
+        i=i+1
 
 if __name__ == '__main__':
     main()
