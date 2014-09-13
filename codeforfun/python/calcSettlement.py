fd2133a5a23a3ad27c5ea4c6c103a02a1704d7af
Scorpiuszjj <520095417@qq.com>
2014-09-13 09:55:20 +0800

    optimize some func

--- codeforfun/python/calcSettlement.py
+++ codeforfun/python/calcSettlement.py
@@ -2,21 +2,14 @@
 #coding:utf8
 import requests
 import re
-
+from datetime import date,timedelta
 
 def getHtml(date):
-    data_result={
-            '爱恋吧':0,
-            '匿爱':0,
-            '风月同城':0,
-            '影缘遇上你':0,
-            '缘来一线':0,
-            '爱看美视频':0,
-            }
-    url='http://e.qq.com/ec/api.php?mod=report&act=campaign&g_tk=782042133&d_p=0.4388993347529322&callback=frameElement.callback&script&g_tk=782042133'
+    data_result=initApp([ '爱恋吧', '匿爱', '风月同城', '影缘遇上你', '缘来一线', '爱看美视频' ])
+    url='http://e.qq.com/ec/api.php?mod=report&act=campaign&g_tk=1007011877&d_p=0.9904019145760685&callback=frameElement.callback&script&g_tk=1007011877'
     headers={
         'Content-Type':'application/x-www-form-urlencoded',
-        'Cookie':'gdt_cm_tag=6f8e3b798fcebb7-10b45e12-48dc-80b9-2c4cd02c942f74785d2fe6; RK=jDEzXtFEPE; pgv_info=ssid=s8009403742; pgv_pvid=1899652405; o_cookie=520095417; pgv_pvi=9923775488; pgv_si=s5717600256; qm_username=520095417; qm_sid=6642b654e822ca306e458b6f7031388b,cl_ydKsiwvwA.; ptui_loginuin=2773324502; pt2gguin=o2773324502; uin=o2773324502; skey=@efOB42lZH; ptisp=cnc; ptcz=4901417f295cd376e954274c4831167737b35ba910cc9d92b18bb42352c5ade3; firsttime=0',
+        'Cookie':' gdt_cm_tag=506de8314d5c8379-19208565-46ec-8d49-189601ab147cedbfc4b83d3; o_cookie=520095417; pgv_pvid=576081791; pgv_info=ssid=s4797060240; RK=Tf07quaQnd; pgv_pvi=5391354880; pgv_si=s1404079104; firsttime=0; zzpaneluin=; zzpanelkey=; verifysession=h028Zd0AxM4yqC592Y9H81VGgbRwRjfJ6G3DdwXm-QBvx1f3SBvQluzrz9_q4Ps3hlSWX9Tv8KW8bqPMc2MV2Kzlw**; ptui_loginuin=2773324502; pt2gguin=o2773324502; uin=o2773324502; skey=@S0MUKKw4z; ptisp=os; ptcz=acb5099a80ee7ff8edb4a9f71c3e056580dc74dc5144beb11712019b3387d316 ',
         }
     data='qzreferrer=http%3A%2F%2Fe.qq.com%2Fatlas%2F153094%2Freport%2Fcampaign%23&datetype=1&format=json&page=1&pagesize=20&fastdate=custom&sdate={date}&edate={date}&searchcname=&reportonly=0&_fastDate=false&callback=frameElement.callback%26script&owner=153094'.format(date=date)
     print date
@@ -29,33 +22,31 @@ def getHtml(date):
     for adv in advs:
         name=adv['campaignname']
         cost=adv['cost']
-        print ' 名称  : {0}   花费 : {1} '.format(name,cost)
+        print ' 名称  : {0}   花费 : {1} '.format(name,int(cost)/100.0 if cost!='-' else cost)
         for app_name in data_result.keys():
             if app_name in name and cost!='-':
                 data_result[app_name]=data_result[app_name]+cost
 
     print '总计'
     for app_name in data_result.keys():
-        print ' 名称 {0}  总花费 {1}'.format(app_name,data_result[app_name])
+        print ' 名称 {0}  总花费 {1}'.format(app_name,int(data_result[app_name])/100.0)
 
+def getDates(date_from,date_to):
+    from_y,from_m,from_d=map(int,date_from.split(','))
+    to_y,to_m,to_d=map(int,date_to.split(','))
+    d_from=date(from_y,from_m,from_d)
+    d_to=date(to_y,to_m,to_d)
+    interval=d_to-d_from
+    return [d_from+timedelta(days=x) for x in xrange(interval.days+1)]
 
-dates={
-    '2014-09-01',
-    '2014-09-02',
-    '2014-09-03',
-    '2014-09-04',
-    '2014-09-05',
-    '2014-09-06',
-    '2014-09-07',
-    '2014-09-08',
-    '2014-09-09',
-    '2014-09-10',
-    '2014-09-11',
-    '2014-09-12',
-        }
+def initApp(nameList):
+    result={}
+    for name in nameList:
+        result[name]=0
+    return result
 
 def main():
-    for date in dates:
+    for date in getDates('2014,9,2','2014,9,4'):
         getHtml(date)
 
 if __name__ == '__main__':
