583232419c134a3c3e74e6a99a46d016eb2013a6
ScorpiusZ <scorpiuszjj@gmail.com>
2015-01-26 11:58:55 +0800

    add save article data to db function

--- codeforfun/python/tool/log_analyze/article_analyze.py
+++ codeforfun/python/tool/log_analyze/article_analyze.py
@@ -4,6 +4,7 @@ import data_analyze as da
 import data_mining as dm
 import api_util
 import id_util
+import configs.db as db
 
 show_format='article_id:{0:10},view_count: {1:10}, product_id:{2:10}\
         view_count:{3:10}, cart_count:{4:6},order_count{5:6}, unique_device:{6}'
@@ -30,6 +31,23 @@ def article_statics(datetime,limit):
         else:
             print show_format.format(id_util.decode_article(article_id),result[article_id],0,0,0,0,unique_device_num)
 
+def save_data(datetime,limit):
+      product_view_counts=da.rowGroupCount(da.getDataFrame('product',datetime),'values')
+      order_product_counts=da.getOrderCounts(datetime)
+      cart_product_counts=da.getCartCount(datetime)
+      result=da.rowGroupCount(da.getDataFrame('article',datetime),'values')
+      article_ids=result.keys()[:limit]
+      for article_id in article_ids:
+          product_ids=api_util.getProductIdInArticle(article_id)
+          if product_ids:
+              for product_id in product_ids:
+                  db.save_article_data(datetime,id_util.decode_article(article_id),result[article_id],product_id,\
+                          da.getProductCounts(product_view_counts,id_util.encode_product(product_id)),\
+                          da.getProductCounts(cart_product_counts,id_util.encode_product(product_id)),\
+                          da.getProductCounts(order_product_counts,id_util.encode_product(product_id)))
+
+          else:
+              db.save_article_data(datetime,id_util.decode_article(article_id),result[article_id],0,0,0,0)
 
 def init(datetime):
     import os
